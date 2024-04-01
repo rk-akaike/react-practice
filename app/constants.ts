d4471c85b0a361b771b995abacb8b8c3c41beb7a
rk-akaike <ramakrishna@akaiketech.com>
2024-04-01 13:16:31 +0530

    Add charts, spline data, and layout utils

---
+++ app/constants.ts
@@ -0,0 +1,9 @@
+import barChart from "@/app/data/bar.json";
+import lineChart from "@/app/data/line.json";
+import pieChart from "@/app/data/pie.json";
+
+export const charts = [
+  { id: "barChart", data: barChart },
+  { id: "lineChart", data: lineChart },
+  { id: "pieChart", data: pieChart },
+];
---
+++ app/utils.ts
@@ -0,0 +1,29 @@
+export const getLayouts = () => {
+  return {
+    lg: [
+      { i: "barChart", x: 0, y: 0, w: 2, h: 2 },
+      { i: "lineChart", x: 2, y: 0, w: 2, h: 2 },
+      { i: "pieChart", x: 4, y: 0, w: 2, h: 2 },
+    ],
+    md: [
+      { i: "barChart", x: 0, y: 0, w: 2, h: 2 },
+      { i: "lineChart", x: 2, y: 0, w: 2, h: 2 },
+      { i: "pieChart", x: 4, y: 0, w: 2, h: 2 },
+    ],
+    sm: [
+      { i: "barChart", x: 0, y: 0, w: 3, h: 2 },
+      { i: "lineChart", x: 3, y: 0, w: 3, h: 2 },
+      { i: "pieChart", x: 6, y: 0, w: 3, h: 2 },
+    ],
+    xs: [
+      { i: "barChart", x: 0, y: 0, w: 4, h: 2 },
+      { i: "lineChart", x: 4, y: 0, w: 4, h: 2 },
+      { i: "pieChart", x: 8, y: 0, w: 4, h: 2 },
+    ],
+    xxs: [
+      { i: "barChart", x: 0, y: 0, w: 6, h: 2 },
+      { i: "lineChart", x: 6, y: 0, w: 6, h: 2 },
+      { i: "pieChart", x: 12, y: 0, w: 6, h: 2 },
+    ],
+  };
+};
